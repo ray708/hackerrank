6cfd8fd27e77130cc4570ee67f6f7d95d117fd87
ray708 <79486128+ray708@users.noreply.github.com>
2022-01-20 10:21:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+            string bases = "";
+            for (int i = 0; i < numero; i++)
+            {
+                bases += "#";
+                string valor = bases.PadLeft(numero);
+                Console.WriteLine(valor);
+            }
+            
+        }
+
+    }
+}

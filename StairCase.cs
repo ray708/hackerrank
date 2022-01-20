c4b0e820235df8d632521490bcf5eab9c2153387
ray708 <79486128+ray708@users.noreply.github.com>
2022-01-20 10:40:56 -0500

    Add files via upload

---
+++ StairCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    
+    public class StairCase
+    {
+
+        public static void DrawingStairCase(int escalones) {
+
+            string altura = "";
+            for (int i = 0; i < escalones; i++)
+            {
+                altura += "#";
+                string escalera = altura.PadLeft(escalones);
+                Console.WriteLine(escalera);
+            }
+
+        }
+        
+}
+}

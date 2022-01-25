a3c4a0880fa6965f5b6881375d19476bd9565b75
ray708 <79486128+ray708@users.noreply.github.com>
2022-01-24 21:05:00 -0500

    Add files via upload

---
+++ BirthdayCakeCandles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    class BirthdayCakeCandles
+    {
+        public static int Candles(List<int> candles) {
+
+            candles.Sort();
+            int lastItem = candles.Count - 1;
+            int numberOfCandles = 1;
+            int maxNumber = candles[lastItem];
+
+            for (int i = lastItem-1; i >=0; i--)
+            {
+                if (candles[i] == maxNumber)
+                { 
+                  numberOfCandles++;
+                  continue;
+                }
+                break; 
+            }
+
+            return numberOfCandles;
+        }
+    }
+}

aa4ea93400953a84953d203ee0694d685e2a7817
ray708 <79486128+ray708@users.noreply.github.com>
2022-01-25 19:01:05 -0500

    Add files via upload

---
+++ TimeConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    class TimeConversion
+    {
+        public static string Conversion(string time) {
+
+            int hour = int.Parse (time.Substring(0,2));
+            string minute = time.Substring(3,2);
+            string second = time.Substring(6,2);
+            char letter = time[8];
+
+            if (letter == 'A' && hour == 12)  hour = 0;
+            
+            if (letter == 'P' && hour !=12) hour += 12;
+
+            if (hour < 10) return string.Format("0{0}:{1}:{2}", hour, minute, second);
+            else return string.Format("{0}:{1}:{2}", hour, minute, second);
+
+        }
+    }
+}

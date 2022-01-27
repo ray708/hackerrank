28bec54b1f60de43c00a3818714025d072d4112f
ray708 <79486128+ray708@users.noreply.github.com>
2022-01-26 19:08:04 -0500

    Add files via upload

---
+++ GradingStudents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class GradingStudents
+    {
+
+        public static List<int> Grading(List<int> lista) {
+
+            for (int i = 0; i < lista.Count ; i++)
+            {
+                int numero = lista.ElementAt(i);
+
+                if (numero > 37)
+                {
+                    if ((numero + 1) % 5 == 0) lista[i] = numero + 1;
+                    if ((numero + 2) % 5 == 0) lista[i] = numero + 2;
+                };
+
+            }
+
+            return lista;
+        
+        }
+    }
+}

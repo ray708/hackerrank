12154c964da30baaf28d2579e51a5ef93b253a21
ray708 <79486128+ray708@users.noreply.github.com>
2022-02-24 10:21:05 -0500

    Add files via upload

---
+++ BreakingRecords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    internal class BreakingRecords
+    {
+        public static List<int> Records(List<int> scores) {
+
+            int max = scores[0];
+            int min = scores[0];
+            int breakMax = 0;
+            int breakMin = 0;
+            
+
+            for (int i = 1; i < scores.Count; i++)
+            {
+                int record = scores[i];
+                if (record > max) {
+                    breakMax++;
+                    max = record;
+                }
+                if (record < min) {
+
+                    breakMin++;
+                    min = record;
+                }
+
+            }
+
+            return new List<int>() { breakMax, breakMin };
+        }
+    }
+}

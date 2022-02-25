8360d28c0c606ff10e020b17a9eb3420f21b4ced
ray708 <79486128+ray708@users.noreply.github.com>
2022-02-25 14:04:22 -0500

    Add files via upload

---
+++ CatsAndAMouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    internal class CatsAndAMouse
+    {
+        public static string catAndMouse(int x, int y, int z) {
+
+            int catA = Math.Abs(z-x);
+            int catB = Math.Abs(z-y);
+            if (catA < catB)  return string.Format("Cat A");
+            if (catA > catB)  return string.Format("Cat B");
+            if (catA == catB) return "Mouse C";
+            return "";
+        }
+    }
+}
---
+++ DivisibleSumPairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackerrank
+{
+    internal class DivisibleSumPairs
+    {
+        public static int divisibleSumpairs(int n, int k, List<int> ar) 
+        {
+            var count = 0;
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                for (int j = i+1; j < ar.Count; j++)
+                {
+                    if ((ar[i] + ar[j]) % k == 0) count++;
+                }
+
+            }
+
+            
+            return count;
+        }
+    }
+}
